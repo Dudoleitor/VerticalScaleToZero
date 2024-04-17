dce0f139a9f9177cd94997c2a3676d651588d3a0
Edoardo Carlotto <edoardo.carlotto@mail.polimi.it>
2024-04-17 22:52:19 +0200

    Updated turnoff policy

--- http_red container/resHandler.go
+++ http_red container/resHandler.go
@@ -2,21 +2,26 @@ package main
 
 import (
   "sync"
-
+  "time"
 	"k8s.io/client-go/kubernetes"
+  "log"
 )
 
 type resourcesHandler struct {
   clientset *kubernetes.Clientset
+  delayS int
   mu sync.Mutex
+  ch chan bool
   inFlightRequests int
   isOn bool
 }
 
 // Create a new request handler
-func newResourceHandler(clientset *kubernetes.Clientset) *resourcesHandler {
+func newResourceHandler(clientset *kubernetes.Clientset, delaySec int) *resourcesHandler {
   return &resourcesHandler{
     clientset: clientset,
+    delayS: delaySec,
+    ch: make(chan bool, 1),
     inFlightRequests: 0,
     isOn: false,
   }
@@ -29,9 +34,18 @@ func (r *resourcesHandler) reqIncoming() {
 
   r.inFlightRequests++
   // Turn on the container
-  if r.isOn == false {
-    r.isOn = true
-    turnOnContainer(r.clientset)
+  if r.inFlightRequests == 1 {  // The first request
+    if r.isOn == false {  // Delay already expired
+      r.isOn = true
+      turnOnContainer(r.clientset)
+    } else {
+      if r.delayS > 0 {
+        select {
+          case r.ch <- false:  // Signal to stop the delay, no need to turn off the container
+          default: log.Println("WARN: Signal channel is full")
+        }
+      }
+    }
   }
 }
 
@@ -46,8 +60,50 @@ func (r *resourcesHandler) reqServed() {
   r.inFlightRequests--
 
   if r.inFlightRequests == 0 { 
-    r.isOn = false
-    // Turn off the container
-    turnOffContainer(r.clientset)
+    if r.delayS > 0 {
+      // Start time to turn off the turn off the container
+      r.turnOffDelay(time.Duration(r.delayS) * time.Second)
+    } else {
+      r.isOn = false
+      turnOffContainer(r.clientset)
+    }
   }
+}
+
+func (r *resourcesHandler) turnOffDelay(delay time.Duration) {
+  interrupted := false
+  localMu := sync.Mutex{}
+
+  // Emptying the channel
+  for len(r.ch) > 0 { 
+    <- r.ch
+  }
+
+  go func() {  // Goroutine to wait for the delay
+    time.Sleep(delay)
+  
+    localMu.Lock()
+    defer localMu.Unlock()
+    if !interrupted {
+      select{
+        case r.ch <- true:
+        default: log.Println("WARN: Signal channel is full")
+      }
+    }
+  } ()
+
+  go func() { // Goroutine to wait for the signal and turn off the container
+    todo := <- r.ch // Waiting for a signal
+    
+    if todo == true {  // Need to turn off the container
+      r.mu.Lock()
+      defer r.mu.Unlock()
+      r.isOn = false
+      turnOffContainer(r.clientset)
+    } else {
+      localMu.Lock()
+      defer localMu.Unlock()
+      interrupted = true
+    }
+  }()
 }
