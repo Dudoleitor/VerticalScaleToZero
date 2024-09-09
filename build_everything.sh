eedf9e2bf60d3ba50747fa40cc04fc09639bd68a
Edoardo Carlotto <edoardo.carlotto@mail.polimi.it>
2024-09-10 00:31:26 +0200

    Installing metricserver

--- build_everything.sh
+++ build_everything.sh
@@ -40,5 +40,6 @@ done
 echo "Appling controller manifest..."
 kubectl apply -f controller/controller.yml
 
-#echo "Installing metrics server"
-#kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+echo "Installing metrics server"
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl patch deployment metrics-server -n kube-system --type=json -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value":"--kubelet-insecure-tls"}]'
